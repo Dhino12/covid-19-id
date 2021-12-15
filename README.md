65eaa073c1783cba3b9dd0a407205f3ca1a0a51d
Mocha Hotto <mochah@localhost.localdomain>
2021-12-15 21:37:06 +0700

    try create covid-19 Web

--- README.md
+++
@@ -1,2 +0,0 @@
-# covid-19-id
-Web Covid-19 di Indonesia Unofficial
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'regenerator-runtime'
+import 'bootstrap/dist/css/bootstrap.min.css' 
+import 'bootstrap/dist/js/bootstrap'
+import "leaflet/dist/leaflet.css"
+import './script/style/main.css'
+import main from './script/view/main.js'
+
+document.addEventListener("DOMContentLoaded", main)
