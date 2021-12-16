136caa8cfe407da87b9776e32e17a17a2026377e
Dhino Rahmad <dhinorahmad0@gmail.com>
2021-12-17 04:08:56 +0700

    fix code with eslint code style airB&B

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -3,7 +3,7 @@ import L from 'leaflet';
 const main = () => {
     const map = L.map('map').setView([51.505, -0.09], 13);
     L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=T3IS26DZaHNhSyDa1msC', {
-        attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+        attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
     }).addTo(map);
 };
 
