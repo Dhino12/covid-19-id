962673e2ab15e54563ce8dbd3f11b2debd744cae
Dhino Rahmad <dhinorahmad0@gmail.com>
2021-12-31 21:21:24 +0700

    add components nav, wave, case in headerElements

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'leaflet/dist/leaflet.css';
 import './script/style/main.css';
+import './script/components/header/NavbarList';
+import './script/components/header/HeaderContent';
+import './script/components/header/WaveHeader';
+import './script/components/header/case-covid-list';
 
 import main from './script/view/main.js';
 
