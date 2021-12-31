34064c43ac159d04ad85eaa4bd4d0ff15fb3f0b4
Dhino Rahmad <dhinorahmad0@gmail.com>
2021-12-31 23:32:46 +0700

    add case list provinsi

---
+++ src/script/components/main/provinsi/provinsi-case-list.js
@@ -0,0 +1,22 @@
+class ProvinsiCaseList extends HTMLElement {
+    constructor() {
+        super();
+        this._caseCovid = ['Terpapar Covid-19', 'Meninggal Dunia', 'Pulih dari Covid-19'];
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = '';
+        this._caseCovid.forEach((caseCovid) => {
+            const caseItemElement = document.createElement('provinsi-case-item');
+            caseItemElement.setAttribute('class', 'col-md-3 ms-3 shadow-blur');
+            caseItemElement.setAttribute('style', 'border-radius: 2vh;');
+            caseItemElement.caseItem = caseCovid;
+        });
+    }
+}
+
+customElements.define('provinsi-case-list', ProvinsiCaseList);
