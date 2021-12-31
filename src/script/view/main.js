1c0c7b9868dd51d8bca5c26b9396bfc569f23a10
Dhino Rahmad <dhinorahmad0@gmail.com>
2021-12-31 23:34:19 +0700

    add extends case-covid-wrapper with case-covid-list

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,6 +1,11 @@
 import L from 'leaflet';
 
 const main = () => {
+    const idWrapper = document.querySelector('#case-wrapper'); // sudah dibuat di file covid-case-wrapper.js
+    const caseCovidList = document.createElement('case-covid-list');
+    caseCovidList.setAttribute('class', 'row');
+    idWrapper.appendChild(caseCovidList);
+
     const map = L.map('map').setView([51.505, -0.09], 13);
     L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=T3IS26DZaHNhSyDa1msC', {
         attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
