0e529751bbf1726563caf0f5001ce59f47415d3a
Dhino Rahmad <dhinorahmad0@gmail.com>
2022-01-04 00:20:35 +0700

    implement province controler

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,4 +1,5 @@
 // import L from 'leaflet';
+import Province from './province/Province';
 
 const main = () => {
     const idWrapper = document.querySelector('#case-wrapper'); // sudah dibuat di file covid-case-wrapper.js
@@ -6,20 +7,11 @@ const main = () => {
     caseCovidList.setAttribute('class', 'row');
     idWrapper.appendChild(caseCovidList);
 
-    const provinceItem = document.querySelectorAll('province-item');
-    const provinces = document.getElementsByTagName('province-list')[0];
-
-    if (document.getElementsByClassName('activee')[0] === undefined) {
-        provinceItem[0].classList.add('activee');
-    }
-
-    provinces.addEventListener('click', (e) => {
-        provinceItem.forEach((element) => {
-            element.classList.remove('activee');
-        });
-        e.target.classList.add('activee');
-    });
-
+    /* province */
+    const ProvinceIndonesia = new Province();
+    ProvinceIndonesia.listProvince();
+    ProvinceIndonesia.provinceName();
+    /* end-province */
     // const map = L.map('map').setView([51.505, -0.09], 13);
     // L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=T3IS26DZaHNhSyDa1msC', {
     //     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
