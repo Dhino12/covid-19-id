50873003f6de44aad2c4b315ab097d4976840cbf
dhino12 <dhinorahmad0@gmail.com>
2022-01-16 23:23:59 +0700

    add geoJson from file static in ./src/geojson/

--- src/script/data/data-province.js
+++ src/script/data/data-province.js
@@ -1,12 +1,23 @@
+/* eslint-disable class-methods-use-this */
 const { default: axios } = require('axios');
 
 class DataProvince {
     #API_PROV = 'https://apicovid19indonesia-v2.vercel.app/api/indonesia';
 
     async dataAllProvince() {
-        const datas = await axios(`${this.#API_PROV}/provinsi`);
+        const datas = await axios(`${this.#API_PROV}/provinsi/more`);
         const listProvinces = await datas.data;
         return listProvinces;
+    }
+
+    async dataGeoJson() {
+        const datasProvince = await axios.get('./geojson/provinceName.json');
+        const nameProvinces = await datasProvince.data;
+        return nameProvinces.map(async (nameProvince) => {
+            const geoJsonProvince = await axios.get(`./geojson/${nameProvince.name}`);
+            const allDataProvince = await geoJsonProvince.data;
+            return allDataProvince;
+        });
     }
 }
 
