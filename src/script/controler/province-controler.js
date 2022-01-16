d2e00d1c6c23a35caedfe3496757ef5df9f552ef
dhino12 <dhinorahmad0@gmail.com>
2022-01-16 23:21:56 +0700

    add geoJson data for provinceGeoJson.js

--- src/script/controler/province-controler.js
+++ src/script/controler/province-controler.js
@@ -1,17 +1,23 @@
 class ProvinceControler {
     #dataAllProvince = [];
 
+    #dataGeoJson = [];
+
     constructor(provinceDatas, provinceView) {
         this.provinceDatas = provinceDatas;
         this.provinceView = provinceView;
     }
 
-    async setNameProvince() {
+    async getNameProvince() {
         this.#dataAllProvince = await this.provinceDatas.dataAllProvince();
-
-        this.provinceView.dataReceiver(this.#dataAllProvince.map((province) => province));
+        this.provinceView.dataReceiver(this.#dataAllProvince);
         this.provinceView.listProvinceName();
         this.provinceView.provinceTitleName();
+    }
+
+    async getProvinceGeoJson() {
+        this.#dataGeoJson = await this.provinceDatas.dataGeoJson();
+        this.provinceView.dataReceiverGeoJson(this.#dataGeoJson);
     }
 }
 
