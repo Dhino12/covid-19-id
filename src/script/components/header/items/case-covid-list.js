9d0f9c0b6ffb9e0644218e7c9b34ed366f39d323
dhino12 <dhinorahmad0@gmail.com>
2022-01-20 20:02:42 +0700

    add setter for data set

--- src/script/components/header/items/case-covid-list.js
+++ src/script/components/header/items/case-covid-list.js
@@ -6,14 +6,20 @@ class CaseCoronaList extends HTMLElement {
     }
 
     connectedCallback() {
-        this.listCaseCorona = ['Total Kasus Covid-19', 'Terpapar Covid-19', 'Meninggal Dunia', 'Pulih dari Covid-19'];
+        this.listCaseCorona = ['Total Kasus Covid-19', 'Di Rawat Covid-19', 'Meninggal Dunia', 'Pulih dari Covid-19'];
+    }
+
+    set dataCovid(covidObj) {
+        this._covidIndo = covidObj;
+        this._dateUpdate = new Date(covidObj.lastUpdate);
+        this._dateUpdate = this._dateUpdate.toDateString();
         this.render();
     }
 
     render() {
         this.innerHTML = '';
 
-        this.listCaseCorona.forEach((cases) => {
+        this.listCaseCorona.forEach((cases, index) => {
             const caseWrapper = document.createElement('case-wrapper');
             caseWrapper.setAttribute('class', 'col-md-3 position-relative br-not-last');
 
@@ -21,13 +27,23 @@ class CaseCoronaList extends HTMLElement {
             caseCovidElement.setAttribute('class', 'text-center py-3');
             caseCovidElement.itemCovid = cases;
 
+            if (index === 0) {
+                caseCovidElement.dataCovidItem = this._covidIndo.positif;
+            } else if (index === 1) {
+                caseCovidElement.dataCovidItem = this._covidIndo.dirawat;
+            } else if (index === 2) {
+                caseCovidElement.dataCovidItem = this._covidIndo.meninggal;
+            } else {
+                caseCovidElement.dataCovidItem = this._covidIndo.sembuh;
+            }
+
             caseWrapper.appendChild(caseCovidElement);
             this.appendChild(caseWrapper);
         });
 
         this.innerHTML += `
         <p class="text-center mt-2 mb-0" style="color: #a5a5a5;">
-            Berdasarkan data 14 Desember 2021
+            Berdasarkan data ${this._dateUpdate}
         </p>
         `;
     }
