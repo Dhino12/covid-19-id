66ed48332f813e88ef539887ea6d91db7c3f6758
dhino12 <dhinorahmad0@gmail.com>
2022-01-20 19:59:41 +0700

    querySelector for indo-case-covid

--- src/script/view/selector/ComponentQuerySelector.js
+++ src/script/view/selector/ComponentQuerySelector.js
@@ -3,16 +3,24 @@ class ComponentQuerySelector {
     #provinceQueryItem = {};
 
     #provinceQuerySelector() {
-        this.#provinceQueryItem.province = document.getElementsByTagName('province-list')[0];
+        this.#provinceQueryItem.provinceList = document.getElementsByTagName('province-list')[0];
         this.#provinceQueryItem.provinceItem = document.querySelectorAll('province-item');
         this.#provinceQueryItem.provinceName = document.querySelector('title-provinsi div h1');
         this.#provinceQueryItem.provinceCaseList = document.querySelector('provinsi-case-list');
         this.#provinceQueryItem.activeeClass = document.querySelector('.activee');
     }
 
+    #indoCovidSelector() {
+        return document.getElementsByTagName('case-covid-list')[0];
+    }
+
     getProvinceQuerySelector() {
         this.#provinceQuerySelector();
         return this.#provinceQueryItem;
+    }
+
+    getIndoCovidSelector() {
+        return this.#indoCovidSelector();
     }
 }
 
